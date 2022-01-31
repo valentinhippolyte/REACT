5a905525cfe23f11c126868c519b4ab8e5d590b8
valentin.hippolyte <valenthippo@gmail.com>
2022-01-31 13:34:38 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
+
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Headline, TextInput } from 'react-native-paper';
 
 export default function App() {
+  const [email, setText] = React.useState("");
+  const [password, setPassword] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Headline style={styles.headline}>Space</Headline>
+      </View>
+      <View style={styles.form}>
+        <View style={styles.formInput}> 
+          <TextInput
+          label="Email"
+            value={email}
+            onChangeText={email => setText(email)}
+            autoComplete={false}
+          /></View>
+       
+        <StatusBar style="auto" />
+        <TextInput
+          label="password"
+          value={password}
+          onChangeText={password => setPassword(password)}
+          autoComplete={false}
+        />
+        <View style={styles.formButton}><Button icon="" mode="contained" onPress={() => console.log('Pressed')}>Log in</Button></View>
+        
+        <StatusBar style="auto" />
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +40,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: "#7950F2",
+    height: '30%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headline: {
+    color: '#ffffff',
+  }, 
+  form:{
+    paddingTop:32,
+    paddingHorizontal:24,
+  },
+  formInput:{
+    paddingBottom:16,
+  },
+  formButton:{
+    paddingTop:16,
+  }
 });
